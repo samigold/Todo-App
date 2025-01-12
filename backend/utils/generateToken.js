6453e96233f4e35ab4878e5a0789bb36e4063cd9
Samuel (Nzube) Madu <51761415+samigold@users.noreply.github.com>
2025-01-12 12:12:55 +0100

    Update generateToken.js
    updated the samesite parameter to allow cookie access

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -6,7 +6,7 @@ const generateToken = (res, userId) => {
     res.cookie('jwt', token, {
         httpOnly: true,
         secure: process.env.NODE_ENV !== 'developement',
-        sameSite: 'strict',
+        sameSite: 'none',
         maxAge: 30 * 24 * 60 * 60 * 1000 //this is 30 days in milliseconds. its 30 days * 24 hours * 60 minutes * 60 seconds * 1000 milliseconds
     });
 };
